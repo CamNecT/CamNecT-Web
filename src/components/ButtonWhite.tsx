339f23d1815184d1b60d814f855d3d5c88538b9e
SangHyun Jung <wjdtkdgus876@naver.com>
2026-07-22 00:51:34 +0900

    style: ButtonWhite 컴포넌트 폰트 수정

--- src/components/ButtonWhite.tsx
+++ src/components/ButtonWhite.tsx
@@ -9,14 +9,14 @@ const ButtonWhite = ({ label, className = '', disabled, ...props}: ButtonWhitePr
   return (
     <button
       disabled={disabled}
-      className={`${twMerge(
-        `max-w-[325px] w-full h-[50px] rounded-[25px] flex items-center justify-center rotate-0 transition
+      className={twMerge(
+        `max-w-[325px] w-full h-[50px] rounded-[25px] flex items-center justify-center text-sb-18 tracking-[-0.72px] rotate-0 transition
         ${disabled
           ? 'bg-gray-150 border-gray-150 text-gray-750 cursor-not-allowed'
           : 'border border-primary bg-white text-primary cursor-pointer active:scale-95 active:brightness-95'
         }`,
         className
-      )} text-SB-18`}
+      )}
       {...props}
     >
       {label}
