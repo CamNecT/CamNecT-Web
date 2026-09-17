8bc36f80d61d6816a133d8d7701f1ae6028011bb
choeunseo <taskcho123@gmail.com>
2026-09-17 17:34:23 +0900

    fix: 정렬 기준 모달 하단 여백 축소

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -32,9 +32,10 @@ const SortSelector = <SortKey extends string>({
       </div>
 
       <BottomSheetModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        {/* 화면 하단을 덮는 모달이므로 내비게이션 높이를 더하지 않고 안전 영역만 확보한다. */}
         <div
           className='flex flex-col gap-[30px] px-[25px] pt-[45px]'
-          style={{ paddingBottom: 'calc(50px + 56px + env(safe-area-inset-bottom))' }}
+          style={{ paddingBottom: 'calc(25px + env(safe-area-inset-bottom, 0px))' }}
         >
           <div className='text-b-18 text-[var(--ColorBlack,#202023)]'>{modalTitle}</div>
           <div className='flex flex-col gap-[20px] px-[7px]'>
